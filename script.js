d288331a4cbce3991adcea154d573f36c6c21b4b
yelasri <elasriyoussef604@gmail.com>
2025-03-24 00:33:32 +0000

    create paddle

--- script.js
+++ script.js
@@ -4,23 +4,40 @@ let paddleWidth = 160;
 let paddleHeight = 20;
 let paddle = {
     x: containerHeight / 2 - paddleWidth / 2,
-    y: containerHeight - paddleHeight - 10
+    y: containerHeight - paddleHeight - 10,
+    width: paddleWidth,
+    height: paddleHeight,
 }
 
 addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.container');
     container.style.width = containerWidth + 'px'
     container.style.height = containerHeight + 'px'
-    createPaddle(container)
+    const paddleDiv = createPaddle(container)
+    addEventListener('keydown', (event) => {
+        movePaddle(event, paddleDiv)
+    })
 })
 
 function createPaddle(container) {
     const paddleDiv = document.createElement('div')
     paddleDiv.className = 'paddle'
     paddleDiv.style.cssText = `
-        width: ${paddleWidth}px;
-        height: ${paddleHeight}px;
+        width: ${paddle.width}px;
+        height: ${paddle.height}px;
         transform: translate(${paddle.x}px, ${paddle.y}px);
     `
     container.append(paddleDiv)
+
+    return paddleDiv
+}
+
+function movePaddle(event, paddleDiv) {
+    if (event.key === 'ArrowLeft') {
+        console.log(event.key)
+    }
+
+    if (event.key === 'ArrowRight') {
+        console.log(event.key)
+    }
 }
