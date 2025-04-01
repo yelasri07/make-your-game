1d46afef1404b121b5fd86dc8156d802e572ac46
yelasri <elasriyoussef604@gmail.com>
2025-04-01 17:35:16 +0000

    add pause menu

--- js/gameController.js
+++ js/gameController.js
@@ -3,8 +3,8 @@ import { resetBall } from "./ball.js"
 export function gameStart() {
     const gameElement = document.querySelector('.game')
     const startElement = document.querySelector('.start') 
-    gameElement.classList.toggle('opacityGame')
-    startElement.classList.toggle('hideStart')
+   gameElement.style.opacity = '1'
+   startElement.style.display = 'none'
 }
 
 export function gameOver(win = true) {
