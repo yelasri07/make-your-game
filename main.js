3135468e44c22c10ed092c88132599b3fdeb39b0
yelasri <elasriyoussef604@gmail.com>
2025-04-11 16:57:04 +0100

    upadte paddle width

--- main.js
+++ main.js
@@ -11,24 +11,3 @@ document.addEventListener('DOMContentLoaded', () => {
     createBricks()
     setupEventListeners()    
 });
-
-// just for test the ball
-addEventListener('keydown', (e) => {
-    let ballElement = document.querySelector('.ball')
-    if (e.key === 'z') {
-        ball.y -= 5
-    } 
-    if (e.key === 's') {
-        ball.y += 5
-    } 
-    if (e.key === 'd') {
-        ball.x += 5
-    } 
-    if (e.key === 'q') {
-        ball.x -= 5
-    } 
-    
-    ballElement.style.cssText += `
-    transform: translate(${ball.x}px, ${ball.y}px);
-    `
-})
