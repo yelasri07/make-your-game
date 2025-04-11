7e69a922ecbc8d99555bd756b5e9cf3dbece1de1
yelasri <elasriyoussef604@gmail.com>
2025-04-11 16:57:15 +0100

    upadte paddle width

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { createPaddle } from "./js/paddle.js"
-import { createBall, updateBallPosition } from "./js/ball.js"
+import { createBall } from "./js/ball.js"
 import { createBricks } from "./js/bricks.js"
 import { setupEventListeners } from "./js/eventHandler.js"
-import { getContainerWidth, ball } from "./js/config.js";
+import { getContainerWidth } from "./js/config.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     getContainerWidth()
