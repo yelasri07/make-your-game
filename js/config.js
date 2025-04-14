f95848c73fe2048abffab562d5e0f10a8044cf55
ismailsayen <sayensma02@gmail.com>
2025-04-14 23:31:00 +0100

    hannn

--- js/config.js
+++ js/config.js
@@ -2,7 +2,6 @@ import { updateBall } from "./ball.js";
 import { updateBricks } from "./bricks.js";
 import { updatePaddle } from "./paddle.js";
 
-
 export let containerWidth = 800;
 export let containerHeight = 800;
 export const paddleWidth = 160;
@@ -11,105 +10,107 @@ export const ballWidth = 20;
 export const ballHeight = 20;
 
 export const paddle = {
-    x: containerWidth / 2 - paddleWidth / 2,
-    y: containerHeight - paddleHeight - 5,
-    width: paddleWidth,
-    height: paddleHeight,
-}
+  x: containerWidth / 2 - paddleWidth / 2,
+  y: containerHeight - paddleHeight - 5,
+  width: paddleWidth,
+  height: paddleHeight,
+};
 
 export const ball = {
-    x: containerWidth / 2 - ballWidth / 2,
-    y: paddle.y - ballHeight * 1.2,
-    width: ballWidth,
-    height: ballHeight,
-    dx: 6 * (Math.random() * 2 - 1),
-    dy: -5,
-    isStarted: false,
-    isReset: true
-}
+  x: containerWidth / 2 - ballWidth / 2,
+  y: paddle.y - ballHeight * 1.2,
+  width: ballWidth,
+  height: ballHeight,
+  dx: 6 * (Math.random() * 2 - 1),
+  dy: -5,
+  isStarted: false,
+  isReset: true,
+};
 
 export const brick = {
-    width: ((containerWidth / 6) - 20) - 3,
-    height: 20,
-    offSetLeft: 20,
-    offSetTop: 20,
-    marginTop: 40,
-    row: 3,
-    column: 6,
-}
+  width: containerWidth / 6 - 20 - 3,
+  height: 20,
+  offSetLeft: 20,
+  offSetTop: 20,
+  marginTop: 40,
+  row: 3,
+  column: 6,
+};
 
 export const scoreBoard = {
-    timer: '03:00',
-    score: 0,
-    lives: 3,
-    minutes: 2,
-    seconds: 60,
-    ms: 10,
-}
+  timer: "03:00",
+  score: 0,
+  lives: 3,
+  minutes: 2,
+  seconds: 60,
+  ms: 10,
+};
 
-addEventListener('resize', () => {
-    const container = document.querySelector('.container');
-    const paddleElement = document.querySelector('.paddle');
-    const ballElement = document.querySelector('.ball');
-    const containerBoundingWidth = container.getBoundingClientRect().width
-    const containerBoundingHeight = container.getBoundingClientRect().height
+addEventListener("resize", () => {
+  const container = document.querySelector(".container");
+  const paddleElement = document.querySelector(".paddle");
+  const ballElement = document.querySelector(".ball");
+  const containerBoundingWidth = container.getBoundingClientRect().width;
+  const containerBoundingHeight = container.getBoundingClientRect().height;
 
-    const paddlePercentX = (paddle.x / containerWidth) * 100;
-    const paddlePercentWidth = (paddle.width / containerWidth) * 100;
-    const paddlePercentHeight = (paddle.height / containerWidth) * 100;
-    const ballPercentX = (ball.x / containerWidth) * 100
-    const ballPercentWidth = (ball.width / containerWidth) * 100;
+  const paddlePercentX = (paddle.x / containerWidth) * 100;
+  const paddlePercentWidth = (paddle.width / containerWidth) * 100;
+  const paddlePercentHeight = (paddle.height / containerWidth) * 100;
+  const ballPercentX = (ball.x / containerWidth) * 100;
+  const ballPercentWidth = (ball.width / containerWidth) * 100;
 
-    brick.width = ((containerBoundingWidth / brick.column) - brick.offSetLeft) - 3;
-    containerWidth = containerBoundingWidth;
-    containerHeight = containerBoundingHeight
+  brick.width = containerBoundingWidth / brick.column - brick.offSetLeft - 3;
+  containerWidth = containerBoundingWidth;
+  containerHeight = containerBoundingHeight;
 
-    paddle.x = (paddlePercentX / 100) * containerWidth;
-    paddle.y = containerHeight - paddleHeight - 5
-    paddle.width = (paddlePercentWidth / 100) * containerWidth;
-    paddle.height = (paddlePercentHeight / 100) * containerWidth;
-    ball.x = (ballPercentX / 100) * containerWidth
-    ball.width = (ballPercentWidth / 100) * containerWidth;
-    ball.height = ball.width
-    paddle.y = containerHeight - ball.height - 5
-    if (ball.isReset) {
-        ball.y = paddle.y - ball.height * 1.2
-    }
+  paddle.x = (paddlePercentX / 100) * containerWidth;
+  paddle.y = containerHeight - paddleHeight - 5;
+  paddle.width = (paddlePercentWidth / 100) * containerWidth;
+  paddle.height = (paddlePercentHeight / 100) * containerWidth;
+  ball.x = (ballPercentX / 100) * containerWidth;
+  ball.width = (ballPercentWidth / 100) * containerWidth;
+  ball.height = ball.width;
+  paddle.y = containerHeight - ball.height - 5;
+  if (ball.isReset) {
+    ball.y = paddle.y - ball.height * 1.2;
+  }
 
-    if (paddle.x <= 0) {
-        paddle.x = 0
-    }
+  if (paddle.x <= 0) {
+    paddle.x = 0;
+  }
 
-    if (paddle.x + paddle.width >= containerWidth) {
-        paddle.x = containerWidth - paddle.width
-    }
+  if (paddle.x + paddle.width >= containerWidth) {
+    paddle.x = containerWidth - paddle.width;
+  }
 
-    updatePaddle(paddleElement)
-    updateBall(ballElement)
-    updateBricks()
-})
+  updatePaddle(paddleElement);
+  updateBall(ballElement);
+  updateBricks();
+});
 
 export function getContainerWidth() {
-    const container = document.querySelector('.container').getBoundingClientRect();
-    containerWidth = container.width;
-    containerHeight = container.height
-    const desiredPercentWidthPaddle = 20;
-    const desiredPercentHeightPaddle = 2.5
-    const desiredPercentWidthBall = 2.5;
+  const container = document
+    .querySelector(".container")
+    .getBoundingClientRect();
+  containerWidth = container.width;
+  containerHeight = container.height;
+  const desiredPercentWidthPaddle = 20;
+  const desiredPercentHeightPaddle = 2.5;
+  const desiredPercentWidthBall = 2.5;
 
-    const newPaddleWidth = (desiredPercentWidthPaddle / 100) * containerWidth;
-    const newPaddleHeight = (desiredPercentHeightPaddle / 100) * containerWidth
-    paddle.width = Math.min(newPaddleWidth, 160);
-    paddle.height = Math.min(newPaddleHeight, 20);
+  const newPaddleWidth = (desiredPercentWidthPaddle / 100) * containerWidth;
+  const newPaddleHeight = (desiredPercentHeightPaddle / 100) * containerWidth;
+  paddle.width = Math.min(newPaddleWidth, 160);
+  paddle.height = Math.min(newPaddleHeight, 20);
 
-    paddle.x = containerWidth / 2 - paddle.width / 2;
-    paddle.y = containerHeight - paddle.height - 5
+  paddle.x = containerWidth / 2 - paddle.width / 2;
+  paddle.y = containerHeight - paddle.height - 5;
 
-    const newBallWidth = (desiredPercentWidthBall / 100) * containerWidth;
-    ball.width = Math.min(newBallWidth, 20);
-    ball.height = ball.width;
-    ball.x = containerWidth / 2 - ball.width / 2
-    ball.y = ball.y = paddle.y - ball.height * 1.2
+  const newBallWidth = (desiredPercentWidthBall / 100) * containerWidth;
+  ball.width = Math.min(newBallWidth, 20);
+  ball.height = ball.width;
+  ball.x = containerWidth / 2 - ball.width / 2;
+  ball.y = ball.y = paddle.y - ball.height * 1.2;
 
-    brick.width = ((container.width / brick.column) - brick.offSetLeft) - 3;
+  brick.width = container.width / brick.column - brick.offSetLeft - 3;
 }
--- js/gameController.js
+++ js/gameController.js
@@ -1,67 +1,70 @@
-import { resetBall, ballAnimationId, updateBallPosition } from "./ball.js"
-import { createBricks } from "./bricks.js"
-import { ball, scoreBoard } from "./config.js"
-import { updateScoreBoard, x, countDown } from "./scoreBoard.js"
-import { movementState } from "./paddle.js"
+import { resetBall, ballAnimationId, updateBallPosition } from "./ball.js";
+import { createBricks } from "./bricks.js";
+import { ball, scoreBoard } from "./config.js";
+import { updateScoreBoard, x, countDown } from "./scoreBoard.js";
+import { movementState } from "./paddle.js";
 
-export function gameStart() {    
-    const gameElement = document.querySelector('.game')
-    const startElement = document.querySelector('.start')
-    const pauseMenu = document.querySelector('.pause-menu')
-    const gameResultMenu = document.querySelector('.game-result-menu')
-
-    gameResultMenu.style.display = 'none'
-    pauseMenu.style.display = 'none'
-    gameElement.style.opacity = '1'
-    startElement.style.display = 'none'
-    
+export function gameStart() {
+  const gameElement = document.querySelector(".game");
+  const startElement = document.querySelector(".start");
+  const pauseMenu = document.querySelector(".pause-menu");
+  const gameResultMenu = document.querySelector(".game-result-menu");
+  const intro = document.querySelector(".intro");
+  gameResultMenu.style.display = "none";
+  pauseMenu.style.display = "none";
+  gameElement.style.opacity = "1";
+  intro.style.opacity = "0";
+  startElement.style.display = "none";
 }
 
-export function gameOver(type) {    
-    const gameResultMenu = document.querySelector('.game-result-menu')
-    const gameElement = document.querySelector('.game')
-    const gameOutcome = document.querySelector('.game-outcome')
-    cancelAnimationFrame(ballAnimationId)
-    clearInterval(x)
-    gameResultMenu.style.display = 'flex'
-    gameElement.style.opacity = '.6'
-    gameOutcome.textContent = `you ${type}`
-    ball.isStarted = true
+export function gameOver(type) {
+  const gameResultMenu = document.querySelector(".game-result-menu");
+  const gameElement = document.querySelector(".game");
+  const gameOutcome = document.querySelector(".game-outcome");
+  cancelAnimationFrame(ballAnimationId);
+  clearInterval(x);
+  gameResultMenu.style.display = "flex";
+  gameElement.style.opacity = ".6";
+  gameOutcome.textContent = `you ${type}`;
+  ball.isStarted = true;
 }
 
 export function gameRestart() {
-    const bricksElement = document.querySelector('.bricks')
-    bricksElement.remove()
-    scoreBoard.timer = '03:00'
-    scoreBoard.score = 0
-    scoreBoard.lives = 3
-    scoreBoard.minutes = 2
-    scoreBoard.seconds = 60
-    scoreBoard.ms = 10
-    updateScoreBoard()
-    resetBall()
-    createBricks()
-    gameStart()
+  const bricksElement = document.querySelector(".bricks");
+  bricksElement.remove();
+  scoreBoard.timer = "03:00";
+  scoreBoard.score = 0;
+  scoreBoard.lives = 3;
+  scoreBoard.minutes = 2;
+  scoreBoard.seconds = 60;
+  scoreBoard.ms = 10;
+  updateScoreBoard();
+  resetBall();
+  createBricks();
+  gameStart();
 }
 
 export function gamePause() {
-    const gameElement = document.querySelector('.game')
-    const pauseMenu = document.querySelector('.pause-menu')
-    const ballElement = document.querySelector('.ball')
-    const gameResultMenu = document.querySelector('.game-result-menu')
-    cancelAnimationFrame(movementState.paddleAnimationId);
-    movementState.paddleAnimationId = null;
-    cancelAnimationFrame(ballAnimationId)
-    clearInterval(x)
-    if (pauseMenu.style.display === 'none' && gameResultMenu.style.display !== 'flex') {        
-        gameElement.style.opacity = '.6'
-        pauseMenu.style.display = 'flex'
-        ball.isStarted = false
-    } else if (pauseMenu.style.display === 'flex' && !ball.isStarted) {
-        ball.isStarted = true
-        ball.isReset = false
-        gameStart()
-        updateBallPosition(ballElement)
-        countDown()
-    }
+  const gameElement = document.querySelector(".game");
+  const pauseMenu = document.querySelector(".pause-menu");
+  const ballElement = document.querySelector(".ball");
+  const gameResultMenu = document.querySelector(".game-result-menu");
+  cancelAnimationFrame(movementState.paddleAnimationId);
+  movementState.paddleAnimationId = null;
+  cancelAnimationFrame(ballAnimationId);
+  clearInterval(x);
+  if (
+    pauseMenu.style.display === "none" &&
+    gameResultMenu.style.display !== "flex"
+  ) {
+    gameElement.style.opacity = ".6";
+    pauseMenu.style.display = "flex";
+    ball.isStarted = false;
+  } else if (pauseMenu.style.display === "flex" && !ball.isStarted) {
+    ball.isStarted = true;
+    ball.isReset = false;
+    gameStart();
+    updateBallPosition(ballElement);
+    countDown();
+  }
 }
