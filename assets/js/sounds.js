4cb990666790a39343ee933bbd5de2193fdc86b3
yelasri <elasriyoussef604@gmail.com>
2025-08-05 10:32:24 +0100

    isyane zdaf rala daf

--- assets/js/sounds.js
+++ assets/js/sounds.js
@@ -1,25 +1,25 @@
 export function LifeLost() {
-  let audio = new Audio("../sounds/life_lost.mp3");
+  let audio = new Audio("assets/sounds/life_lost.mp3");
   audio.play();
 }
 
 export function PaddleHit() {
-  let audio = new Audio("../sounds/paddle_hit.mp3");
+  let audio = new Audio("assets/sounds/paddle_hit.mp3");
   audio.play();
 }
 export function Win() {
-  let audio = new Audio("../sounds/win.mp3");
+  let audio = new Audio("assets/sounds/win.mp3");
   audio.play();
 }
 export function BrickHit() {
-  let audio = new Audio("../sounds/brick_hit.mp3");
+  let audio = new Audio("assets/sounds/brick_hit.mp3");
   audio.play();
 }
 export function Wall() {
-  let audio = new Audio("../sounds/wall.mp3");
+  let audio = new Audio("assets/sounds/wall.mp3");
   audio.play();
 }
 export function GameOver() {
-  let audio = new Audio("../sounds/gameover.mp3");
+  let audio = new Audio("assets/sounds/gameover.mp3");
   audio.play();
 }
